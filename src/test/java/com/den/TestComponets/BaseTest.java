46852c37c3058e46db623bb34bceb8d7b0bca794
Denys Pugachov <pugachovdenys@gmail.com>
2025-04-05 19:42:13 +0200

    166:refactor project folder structure (all test in test folder)

---
+++ src/test/java/com/den/TestComponets/BaseTest.java
@@ -0,0 +1,4 @@
+package com.den.TestComponets;
+
+public class BaseTest {
+}
--- src/test/java/com/den/pageobjects/LandingPage.java
+++ src/test/java/com/den/pageobjects/LandingPage.java
@@ -1,7 +1,6 @@
 package com.den.pageobjects;
 
 import com.den.AbstractComponents.AbstractComponent;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
